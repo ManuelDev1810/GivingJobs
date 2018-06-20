a0920a3ad8f7888316f19ad64aaaeb3763f17dc4
ManuelDev1810 <manueldev1810@gmail.com>
2018-06-20 19:20:39 -0400

    Putting the pagination

--- ReactJs/giving-jobs/src/components/Jobs/Jobs.js
+++ ReactJs/giving-jobs/src/components/Jobs/Jobs.js
@@ -6,16 +6,24 @@ class Jobs extends Component{
     constructor(props){
         super(props);
         this.state = {
-          search: ''
+          search: '',
+          currentPage: 1,
+          jobsPerPage: 20
         }
         this.updateSearch = this.updateSearch.bind(this)
+        this.handleClick = this.handleClick.bind(this);
     }
 
     updateSearch(event){
         this.setState({search: event.target.value.substr(0,20)})
     }
 
-    static isAdminLabels(admin){
+    handleClick(event) {
+      console.log(event)
+     this.setState({currentPage:Number(event.target.id)})
+    }
+
+   isAdminLabels(admin){
       if(admin){
         return(
           <div>
@@ -26,74 +34,96 @@ class Jobs extends Component{
       }
     }
 
-     static jobComponent(id, props){
+    jobComponent(id, props){
       props.history.push({
         pathname: '/Job',
         state: {id : id}
       })
     }
     
-    static date(date){
+    date(date){
       var dateCreated = new Date(date);
       return(dateCreated.toDateString())
     }
 
-     static renderJobsTable(jobs, state, props){
-       let  filteredJobs = jobs.filter(
-            job => {
-              //Return whatever that is not -1, indexof returns -1 if not find anything.. 
-              //So if you cannot get this particular name of the job DONT RETURN ANYTHING
-              return (job.location.toLowerCase().indexOf(state.search.toLowerCase()) !== -1  || 
-                      job.position.toLowerCase().indexOf(state.search.toLowerCase()) !== -1 ||
-                      job.company.toLowerCase().indexOf(state.search.toLowerCase()) !== -1 ||
-                      job.category.name.toLowerCase().indexOf(state.search.toLowerCase()) !== -1 ||
-                      Jobs.date(job.date).toLowerCase().indexOf(state.search.toLowerCase()) !== -1
-                    );
-            }
-       ).reverse();
-        return(
-          <div className="row justify-content-between">
-              <h2 className="col-12 mito">Jobs</h2>
-              <table className="table col-8">
-    
-                  <thead className="thead-dark">
-                    <tr>
-                      <th scope="col">Location</th>
-                      <th scope="col">Position</th>
-                      <th scope="col">Company</th>
-                      <th scope="col">Category</th>
-                      <th scope="col">Date</th>
-                    </tr>
-                  </thead>
+    renderJobsTable(jobs, state, props){
+      let  filteredJobs = jobs.filter(
+          job => {
+            //Return whatever that is not -1, indexof returns -1 if not find anything.. 
+            //So if you cannot get this particular name of the job DONT RETURN ANYTHING
+            return (job.location.toLowerCase().indexOf(state.search.toLowerCase()) !== -1  || 
+                    job.position.toLowerCase().indexOf(state.search.toLowerCase()) !== -1 ||
+                    job.company.toLowerCase().indexOf(state.search.toLowerCase()) !== -1 ||
+                    job.category.name.toLowerCase().indexOf(state.search.toLowerCase()) !== -1 ||
+                    this.date(job.date).toLowerCase().indexOf(state.search.toLowerCase()) !== -1
+                  );
+          }
+      ).reverse();
 
-                  <tbody>
-                    {filteredJobs.map(job => 
-                          <tr onClick={() => this.jobComponent(job.id, props)} key={job.id}>
-                            <td>{job.location}</td>
-                            <td>{job.position}</td>
-                            <td>{job.company}</td>
-                            <td>{job.category.name}</td>
-                            <td>{Jobs.date(job.date)}</td>
-                          </tr>
-                    ).reverse()}
-                  </tbody>
-              </table>
-                  <div className="card col-3">
-                    <div className="card-body d-flex flex-column">
-                        <h3 className="card-title">Categories</h3>
-                        {props.categories.map(category => 
-                          <Link key={category.id} className="btn btn-success mb-3" to={{pathname: '/Category', state:{id: category.id}}}>{category.name}</Link>
-                        )}
-                    </div>
-                </div> 
-          </div>
-        );
+      //Logic for displaying current jobs
+      const indexOfLastJob = state.currentPage * state.jobsPerPage;
+      const indexOfFirstJob = indexOfLastJob - state.jobsPerPage;
+      const currentJobs = filteredJobs.reverse().slice(indexOfFirstJob,indexOfLastJob)
+
+      //Logic for displating page numbers
+      const pageNumbers = [];
+      for(let i = 1; i <= Math.ceil(filteredJobs.length / state.jobsPerPage); i++){
+        pageNumbers.push(i)
       }
+
+      return(
+        <div className="row justify-content-between">
+            <h2 className="col-12 mito">Jobs</h2>
+            <table className="table col-8">
+
+            {/* Jobs */}
+            
+                <thead className="thead-dark">
+                  <tr>
+                    <th scope="col">Location</th>
+                    <th scope="col">Position</th>
+                    <th scope="col">Company</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Date</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {currentJobs.map(job => 
+                        <tr onClick={() => this.jobComponent(job.id, props)} key={job.id}>
+                          <td>{job.location}</td>
+                          <td>{job.position}</td>
+                          <td>{job.company}</td>
+                          <td>{job.category.name}</td>
+                          <td>{this.date(job.date)}</td>
+                        </tr>
+                  )}
+                </tbody>
+                      
+                {pageNumbers.map( page =>
+                  <a className="mr-3" key={page} id={page} onClick={this.handleClick}>{page}</a>
+                )}
+            </table>
+            
+            {/* Cateogires */}
+
+            <div className="card col-3">
+                  <div className="card-body d-flex flex-column">
+                      <h3 className="card-title">Categories</h3>
+                      {props.categories.map(category => 
+                        <Link key={category.id} className="btn btn-success mb-3" to={{pathname: '/Category', state:{id: category.id}}}>{category.name}</Link>
+                      )}
+                  </div>
+            </div> 
+            
+        </div>
+      );
+    }
       
       render() {
         let content = this.props.loading
         ? <p><em>Loading...</em></p>
-        : Jobs.renderJobsTable(this.props.jobs, this.state, this.props);
+        : this.renderJobsTable(this.props.jobs, this.state, this.props);
         return(
           <div>
             <input className="mb-2 form-control" type="text" name="nombre" placeholder="Search"  onChange={this.updateSearch} />
